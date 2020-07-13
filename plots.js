b3bc29ef11fb2da5c017b9d387b36225f3814810
Quinton Wandzura <quinton.wandzura2@ucalgary.ca>
2020-07-12 18:38:56 -0600

    Uploading Assignment

--- plots.js
+++ plots.js
@@ -13,6 +13,7 @@ function init() {
   })}
   
 init();
+optionChanged(940);
 
 function optionChanged(newSample) {
     buildMetadata(newSample);
@@ -37,12 +38,72 @@ function buildMetadata(sample) {
     });
 };
 
-function buildCharts(sample) {
+function buildCharts(personID) {
   d3.json("samples.json").then((data) => {
     var sample = data.samples;
-    console.log(sample);
-    var resultArray = sample.filter(sampleObj => sampleObj.id == sample);
-    console.log(resultArray);
+    var metadata = data.metadata;    
+    var arrayMeta = metadata.filter(row => row.id == personID);
+    var person = sample.filter(row => row.id == personID);
+    personObj = person[0];
+    metaArray = arrayMeta[0];
 
+    values = personObj.sample_values.slice(0,10);
+
+    otuIds = personObj.otu_ids.slice(0,10).map(function(id){
+      var text = "OTU "+ id;
+      return text;
+    });
+
+    otuLabels = personObj.otu_labels.slice(0,9);
+
+    var traceBar = {
+      x: values,
+      y: otuIds,
+      type: "bar",
+      orientation: 'h',
+      text: otuLabels
+    };
+
+    var layoutBar = {
+    yaxis:{
+        autorange:'reversed'
+      }
+    }
+   Plotly.newPlot("bar", [traceBar], layoutBar);
+
+   wfreq = metaArray.wfreq;
+
+   var dataGauge = [
+    {
+      title: {text: "Belly Button Washing Frequency<br>Scrubs per Week"},
+      domain: { x: [0, 1], y: [0, 1] },
+      value: wfreq,
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: { axis: { range: [null, 9] }}
+    }
+    ];
+
+    var layoutGauge = { autosize : "True"};
+
+    Plotly.newPlot('gauge', dataGauge, layoutGauge);
+
+    var trace1 = {
+      x: personObj.otu_ids,
+      y: personObj.sample_values,
+      text: personObj.otu_labels,
+      mode: 'markers',
+      marker: {
+        size: personObj.sample_values,
+        color: personObj.otu_ids
+      }
+    };
+    var dataScatter = [trace1];
+
+    var layoutScatter = {
+      xaxis : { title: { text: "OTU ID"}}
+    };
+
+    Plotly.newPlot('bubble', dataScatter, layoutScatter);
   });
 }
